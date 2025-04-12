f703cba0f1b969e7389629e69602f3bf0991678a
IagoAlves1 <iago.s.alves.sss@gmail.com>
2025-04-12 18:09:52 -0300

    bug(fix) Ajustado rotas do endpoint user

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from api.v1.endpoints import user
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return{"mensagem": "API funcionando!"}
+def read_root():
+    return {"message": "API funcionando!"}
+
+app.include_router(user.router, prefix="/api/v1/endpoints")
