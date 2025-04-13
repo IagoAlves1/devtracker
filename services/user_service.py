ff063b2a62802f8b52d2e7abeae38bd60fbced6d
IagoAlves1 <iago.s.alves.sss@gmail.com>
2025-04-12 22:12:02 -0300

    (Security) Incluído hash de senha

--- services/user_service.py
+++ services/user_service.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
 from models.user import User
 from schemas.user import UserCreate
+from utils.security import hash_password
 
 def create_user(db, name: str, email: str, password: str):
-    user = User(name=name, email=email, password=password)
+    hashed_pw = hash_password(user.password)
+    user = User(name=name, email=email, password=hashed_pw)
     db.add(user)
     db.commit()
     db.refresh(user)
