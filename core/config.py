6173a6fd0b4d6b2c7975bea4c26beb731acff484
IagoAlves1 <iago.s.alves.sss@gmail.com>
2025-05-04 18:47:13 -0300

    Configuração do CI com Docker

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 
 env_path = Path(__file__).parent.parent / '.env'
 
-load_dotenv(dotenv_path=env_path)
+load_dotenv(dotenv_path=".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
